0b407278717dc72ff5de335dcec27d19308aa5df
Nam Nguyen <namnguyen20999@gmail.com>
2023-08-02 05:35:40 +0700

    Web App

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import time as t
+import pathlib
+import os.path
+from sklearn.model_selection import train_test_split
+from pmdarima import auto_arima
+from datetime import time, datetime
+
+# X = df.iloc[:, :-1]
+# y = df['Giá']
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+# model = auto_arima(y_train, start_p=1, start_q=1)
+
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file:
+    with st.spinner('Loading data...'):
+        t.sleep(2)
+    st.success('Success!')
+    # EDA and preprocessing for time series data
+    df = pd.read_csv(uploaded_file, index_col="Ngày", parse_dates=True).asfreq('D')
+    df = pd.read_csv('./coffee_data.csv', index_col="Ngày", parse_dates=True).asfreq('D')
+    df['Tên_mặt_hàng'].fillna('Cà phê Robusta nhân xô', inplace=True)
+    df['Đơn_vị_tính'].fillna('VNĐ/kg', inplace=True)
+    df['Loại_giá'].fillna('Thu mua', inplace=True)
+    df['Giá'].fillna(df['Giá'].resample('1D').mean().ffill(), inplace=True)
+    df['Loại_tiền'].fillna('VNĐ', inplace=True)
+    df['Đơn_vị_tính'] = 'VNĐ/kg'
+    
+    def separate_year_month_day(date_obj):
+        # Extract year, month, day from the datetime object
+        year = date_obj.year
+        month = date_obj.month
+        day = date_obj.day
+    
+        return datetime(year, month, day)
+
+    date_range = st.slider(
+    "What date range do you want to see?",
+    min_value=separate_year_month_day(df.index[0]),
+    value=(separate_year_month_day(df.index[0]), separate_year_month_day(df.index[-1]))
+    )
+
+    st.write(df.loc[date_range[0]:date_range[1]])
+
+
